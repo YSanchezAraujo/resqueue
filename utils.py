3584588d8a99f14ca0b1b74e3ec14179d133fa42
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2018-01-24 17:19:55 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ from subprocess import call
 
 def reformat_input(files, sep=" "):
     outstr = '"'
+    
     if isinstance(files, list):
         for idx, f in enumerate(files):
             if idx+1 != len(files):
@@ -10,6 +11,7 @@ def reformat_input(files, sep=" "):
             else:
                 outstr += f
         outstr += '"'
+        
     elif isinstance(files, str):
         flist = files.split(" ")
         for idx, f in enumerate(flist):
@@ -18,6 +20,7 @@ def reformat_input(files, sep=" "):
             else:
                 outstr += f
         outstr += '"'
+        
     return outstr
 
 def make_call_cmd(iter_items, resource, type_script):
@@ -25,8 +28,10 @@ def make_call_cmd(iter_items, resource, type_script):
     resource: dict, resources for sbatch call
     """
     script_dict = dict(R="Rscript --vanilla", py="python", sh="bash")
+    
     if type_script not in script_dict.keys():
         raise ValueError("type_script must be one of :{}".format(script_dict.keys()))
+        
     script_call = [script_dict[type_script]]
     fmt_place_holders = ["{{{}}}".format(key) for key in iter_items.keys()]
     cmdstr = ["#!/bin/bash", "#SBATCH --mem={mem}G", "#SBATCH -c {cores}", "#SBATCH --time={time}"]
