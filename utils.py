187f7eebf9c3af3fed13c0de5ad8e8d2220ba167
Yoel Sanchez Araujo <yoelsanchezaraujo@nat-oitwireless-inside-vapornet100-a-21200.Princeton.EDU>
2018-01-25 11:08:32 -0500

    minor chanages, no updates reall

--- utils.py
+++ utils.py
@@ -1,9 +1,14 @@
 import os
-from subprocess import call
+
+"""
+next steps:
+figure out the plan for the iterator, have it be
+general enough so that it can handle cross products between
+sets of iterables, and well as sets of lists of iterables 
+"""
 
 def reformat_input(files, sep=" "):
     outstr = '"'
-    
     if isinstance(files, list):
         for idx, f in enumerate(files):
             if idx+1 != len(files):
@@ -11,7 +16,6 @@ def reformat_input(files, sep=" "):
             else:
                 outstr += f
         outstr += '"'
-        
     elif isinstance(files, str):
         flist = files.split(" ")
         for idx, f in enumerate(flist):
@@ -20,7 +24,6 @@ def reformat_input(files, sep=" "):
             else:
                 outstr += f
         outstr += '"'
-        
     return outstr
 
 def make_call_cmd(iter_items, resource, type_script):
@@ -28,13 +31,10 @@ def make_call_cmd(iter_items, resource, type_script):
     resource: dict, resources for sbatch call
     """
     script_dict = dict(R="Rscript --vanilla", py="python", sh="bash")
-    
     if type_script not in script_dict.keys():
         raise ValueError("type_script must be one of :{}".format(script_dict.keys()))
-        
     script_call = [script_dict[type_script]]
     fmt_place_holders = ["{{{}}}".format(key) for key in iter_items.keys()]
     cmdstr = ["#!/bin/bash", "#SBATCH --mem={mem}G", "#SBATCH -c {cores}", "#SBATCH --time={time}"]
-    Scmd = " ".join(script_call + fmt_place_holders).format(**iter_items)
-    cmd = cmdstr + [Scmd]
-    return "\n".join(cmd).format(**resource)
+    scmd = " ".join(script_call + fmt_place_holders).format(**iter_items)
+    return "\n".join(cmdstr + [scmd]).format(**resource)
