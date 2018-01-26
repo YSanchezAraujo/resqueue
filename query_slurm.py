b9e9b4da3aa4103b247e9eade9a49fdecfe803f6
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2018-01-26 17:25:22 -0500

    Update query_slurm.py

--- query_slurm.py
+++ query_slurm.py
@@ -1,4 +1,5 @@
 import subprocess
+from collections import Counter
 
 def command(cmd, sep=" "):
     """cmd: string or list, the command you want to execute,
@@ -11,8 +12,20 @@ def command(cmd, sep=" "):
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
     out, err = process.communicate()
     return out.decode("utf-8").split("\n")
-   
+
 """ can use the above to check jobids, as a first use case, try
 to check your jobs and count how many you have running, will somehow
 have to limit it to only jobs queued up within slurm_handler
+Counter will be used later..
 """
+
+def squeue_handle(piped_input):
+    """piped_input: list, this is the output from the command function
+    intended for squeue but maybe will be used for other commands
+    """
+    for idx, line in enumerate(piped_input):
+        piped_input[idx] = line.split()
+    if not piped_input[-1]:
+        del piped_input[-1]
+    return piped_input
+
