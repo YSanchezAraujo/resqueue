ae14c650f756af821446ffd5a6012c2439bb4617
YSanchezAraujo <yoelsanchez006@gmail.com>
2018-01-29 20:49:37 -0500

    update

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name="slurm_handler",
     packages = ["slurm_handler"],
-    version="0.0.1",
+    version="0.0.12",
     description = "job submission in python using the slurm job manager", 
-    url="https://github.com/YSanchezAraujo",
+    url="https://github.com/YSanchezAraujo/slurm_handler",
     author="Yoel Sanchez Araujo",
     author_email="yoelsanchez006@gmail.com",
-    download_url = "https://github.com/YSanchezAraujo/archive/0.0.1.tar.gz",
+    download_url = "https://github.com/YSanchezAraujo/archive/0.0.12.tar.gz",
     keywords = ["clusters", "slurm"],
     classifiers = [],
 )
