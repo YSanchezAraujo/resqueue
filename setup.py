083a4a3197e903e6d8deaa7eaf43fe5893d657ff
YSanchezAraujo <yoelsanchez006@gmail.com>
2018-01-29 20:31:30 -0500

    packaging..

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="slurm_handler",
+    packages = ["slurm_handler"],
+    version="0.0.1",
+    description = "job submission in python using the slurm job manager", 
+    url="https://github.com/YSanchezAraujo",
+    author="Yoel Sanchez Araujo",
+    author_email="yoelsanchez006@gmail.com",
+    download_url = "https://github.com/YSanchezAraujo/archive/0.0.1.tar.gz",
+    keywords = ["clusters", "slurm"],
+    classifiers = [],
+)
+
+
