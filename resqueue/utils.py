4fc118ecdd3aa21e0a6b3e7a5109b4a718dd235c
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2018-06-29 09:06:53 -0400

    Create utils.py

---
+++ resqueue/utils.py
@@ -0,0 +1,7 @@
+import os
+import subprocess
+
+def shell(cmd_split):
+    process = subprocess.Popen(cmd_split, stdout=subprocess.PIPE)
+    out, err = process.communicate()
+    return out.decode("utf-8").split("\n")
